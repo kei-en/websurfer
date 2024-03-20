6a00a3010477b9c254807bb9060b46044a96254b
kei-en <njuguna0karanja@gmail.com>
2024-03-20 18:11:00 +0300

    Update welcome message in categories component

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useCallback, useState } from "react";
-import { InputOption } from "./ui/inputOption";
+import { InputOption } from "./ui/inputoption";
 import { MdKitesurfing } from "react-icons/md";
 
 export default function Categories({ categories }: { categories: Array<any> }) {
@@ -79,10 +79,11 @@ export default function Categories({ categories }: { categories: Array<any> }) {
               Welcome to Surf<del>Bored</del>!
             </h2>
             <p>
-              Click the Next Site button and get a random website. It's that
-              easy. If you want, you can select categories to explore below. If
-              you don't select any categories, all will be used to find websites
-              for you. Categories can be changed later on from the menu.
+              Click the Next Site button and get a random website. It&apos;s
+              that easy. If you want, you can select categories to explore
+              below. If you don&apos;t select any categories, all will be used
+              to find websites for you. Categories can be changed later on from
+              the menu.
             </p>
             <button
               onClick={() => getRandomSite()}
