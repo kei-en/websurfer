32750af199daccac289979be8a5ac7a978cac23b
kei-en <njuguna0karanja@gmail.com>
2024-10-09 17:19:25 +0300

    Update rules component content, replace surfbored with websurfer

--- components/rules.tsx
+++ components/rules.tsx
@@ -2,22 +2,22 @@ import Accordion from "./ui/accordion";
 import { rules } from "@/data";
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 import { ImBlocked } from "react-icons/im";
-import { FaInfo } from "react-icons/fa";
+import { FaInfoCircle } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Rules() {
   return (
-    <section className="relative w-[98%] md:w-[95%] lg:w-5/6 h-5/6 mx-auto mt-8 mb-0 bg-cyan-950 text-white rounded-md md:rounded-lg p-4">
+    <section className="relative w-[98%] md:w-[95%] lg:w-5/6 h-5/6 mx-auto mt-20 mb-12 border border-slate-800 bg-slate-950 text-white rounded-md md:rounded-lg p-4">
       <div className="my-8 lg:m-8 w-fit">
         <h2 className="text-2xl font-semibold py-4">
-          Submit a website to Surf<del>Bored</del>
+          Submit a website to Websurfer
         </h2>
         <p>
-          To submit a website to SurfBored fill in the form at the bottom of
-          this page. Please note, that SurfBored prioritizes{" "}
+          To submit a website to Websurfer fill in the form at the bottom of
+          this page. Please note, that Websurfer prioritizes{" "}
           <b>Quality over Quantity</b> and has <b>strict rules</b> for what
           websites can be submitted. Submissions not following these rules will
-          be rejected. We recommend browsing SurfBored for a while to get a
+          be rejected. We recommend browsing Websurfer for a while to get a
           feeling of what we want on this site.
         </p>
       </div>
@@ -53,23 +53,14 @@ export default function Rules() {
           </div>
         </div>
       )}
-      <div className="flex justify-evenly mx-5 my-2 rounded-md border border-gray-50 p-8 bg-sky-400 bg-opacity-50">
-        <FaInfo className="w-6 h-6" />
+      <div className="mx-4 my-2 rounded-md border border-gray-50 p-8 bg-sky-400 bg-opacity-50">
+        <FaInfoCircle className="w-3 h-3 -ml-3" />
         <p>
-          Before submitting a lot of sites to SurfBored,&nbsp;
+          Before submitting sites to Websurfer,&nbsp;
           <Link className="font-semibold underline" href="/surf">
             surf through the existing Websites
           </Link>{" "}
-          for a while to get an impression what is already there.
-        </p>
-      </div>
-      <div className="my-8 lg:m-8 w-fit">
-        <h2 className="text-2xl font-semibold py-4">
-          Submit your own Website!
-        </h2>
-        <p>
-          SurfBored encourages self-promotion! Of course, as long as your site
-          is within the given rules. So, go for it.
+          for a while to get an impression of what is already there.
         </p>
       </div>
     </section>
