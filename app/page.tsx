8a9ae77f65bb6e489e33430cf7eb4f1cf2eeabdf
kei-en <njuguna0karanja@gmail.com>
2024-10-20 17:17:42 +0300

    Add cookie handling function, import footer component into the main main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+import { Footer } from "@/components/footer";
 import { IntroHeader } from "@/components/ui/intro";
+import { categories } from "@/data";
+import { cookies } from "next/headers";
 
 export default function Home() {
+  const getCategories = async (formData: FormData) => {
+    "use server";
+    let body = formData.getAll("category");
+
+    if (body.length < 1) {
+      for (const entry of categories) {
+        body.push(entry.id);
+      }
+    }
+
+    cookies().set({
+      name: "categories",
+      value: JSON.stringify(body),
+      httpOnly: true,
+      path: "/surf",
+      maxAge: 3600,
+      secure: true,
+    });
+  };
+
   return (
-    <section>
+    <section className="bg-slate-950">
       <IntroHeader />
+      <Footer />
     </section>
   );
 }
