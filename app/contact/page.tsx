f4dd20b36390f5acab886d1868daf1ae5a12c551
kei-en <njuguna0karanja@gmail.com>
2024-10-27 17:10:38 +0300

    Create contact page, include FAQ, ContactForm and Footer components in it

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import { ContactForm } from "@/components/ContactForm";
+import FAQ from "@/components/FAQ";
+import { Footer } from "@/components/footer";
+
+export default function Contact() {
+  return (
+    <div>
+      <FAQ />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+}
