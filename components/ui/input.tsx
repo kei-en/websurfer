2ff28fff9e6a9c41ce79ef85d1579274dca8319d
kei-en <njuguna0karanja@gmail.com>
2024-10-30 17:36:21 +0300

    Modify styles including functionality for when the user is using eiter dark or light mode

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -38,8 +38,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            `flex h-10 w-full border-none bg-slate-800 dark:bg-slate-800 text-white dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
-          file:text-sm file:font-medium placeholder:text-slate-400 dark:placeholder-text-slate-400 
+            `flex h-10 w-full border-none bg-slate-200 dark:bg-slate-800 text-slate-950 dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+          file:text-sm file:font-medium placeholder:text-slate-700 dark:placeholder:text-slate-400 
           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-slate-600 dark:focus-visible:ring-slate-600
            disabled:cursor-not-allowed disabled:opacity-50
            dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
