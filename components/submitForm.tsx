8227c1f61250c0151e87da1e43cffb2d210ca18e
kei-en <njuguna0karanja@gmail.com>
2024-10-31 17:16:11 +0300

    Modify styles including functionality for when the user is using eiter dark or light mode

--- components/submitForm.tsx
+++ components/submitForm.tsx
@@ -23,7 +23,7 @@ export function SubmitForm({
   return (
     <div
       id={id}
-      className="w-[95%] lg:w-5/6 mx-auto mb-2 md:mb-4 rounded-md md:rounded-lg p-4 pt-2 px-10 md:p-8 md:pt-4 md:px-16 shadow-input border border-slate-800 bg-slate-950 opacity-95">
+      className="w-[95%] lg:w-5/6 mx-auto mb-2 md:mb-4 rounded-md md:rounded-lg p-4 pt-2 px-10 md:p-8 md:pt-4 md:px-16 shadow-input border border-slate-800 bg-slate-300 dark:bg-slate-950 opacity-95">
       <div className="my-8 mx-0  w-fit">
         <h2 className="text-2xl font-semibold py-4">
           Submit your own Website!
@@ -65,9 +65,7 @@ export function SubmitForm({
           />
         </LabelInputContainer>
         <LabelInputContainer className="my-4">
-          <h3 className="mb-4 font-semibold text-sm text-white">
-            Select a category
-          </h3>
+          <h3 className="mb-4 font-semibold text-sm">Select a category</h3>
           <ul className="grid md:grid-cols-2 auto-cols-auto gap-4 md:p-4">
             {categories.map((category) => (
               <li key={category.id}>
@@ -88,7 +86,7 @@ export function SubmitForm({
           </ul>
         </LabelInputContainer>
         <button
-          className="bg-gradient-to-br relative group/btn from-slate-950 via-slate-800 dark:from-slate-600 dark:via-slate-800 dark:to-slate-600 to-slate-800 block dark:bg-slate-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="bg-gradient-to-br relative group/btn from-slate-400 via-slate-300 dark:from-slate-600 dark:via-slate-800 dark:to-slate-600 to-slate-400 block dark:bg-slate-800 w-full rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit">
           Submit site &rarr;
           <BottomGradient />
