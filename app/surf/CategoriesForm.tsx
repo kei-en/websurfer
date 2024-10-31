a2f3ec57e710ec626c3521c322205076a7d8136a
kei-en <njuguna0karanja@gmail.com>
2024-10-31 17:16:11 +0300

    Modify styles including functionality for when the user is using eiter dark or light mode

--- app/surf/CategoriesForm.tsx
+++ app/surf/CategoriesForm.tsx
@@ -26,7 +26,7 @@ export async function CategoriesForm() {
   };
 
   return (
-    <section className="md:w-[95%] lg:w-5/6 h-5/6 mx-auto mt-8 mb-0 md:my-8 bg-cyan-950 text-white rounded-md rounded-b-none md:rounded-lg p-4">
+    <section className="md:w-[95%] lg:w-5/6 h-5/6 mx-auto mt-8 mb-0 md:my-8 border border-slate-700 bg-slate-300 dark:bg-slate-950 rounded-md rounded-b-none md:rounded-lg p-4">
       <div className="my-8 lg:m-8 w-fit">
         <h2 className="text-2xl font-semibold py-4">Welcome to Websurfer!</h2>
         <p>
@@ -36,7 +36,7 @@ export async function CategoriesForm() {
           for you. Categories can be changed later on from the menu.
         </p>
         <form action={getCategories}>
-          <button className="flex w-3/4 md:w-1/3 lg:w-1/4 justify-center items-center text-white mx-auto mt-16 md:mt-20 p-4 rounded-lg bg-[#0E96C1] hover:bg-[#097191] cursor-pointer">
+          <button className="flex w-3/4 md:w-1/3 lg:w-1/4 justify-center items-center text-white mx-auto mt-16 md:mt-20 p-4 rounded-lg bg-sky-700 hover:bg-sky-600 cursor-pointer">
             <MdKitesurfing className="text-2xl mr-2" />
             <span className="text-xl font-medium">Start Surfing</span>
           </button>
@@ -57,7 +57,7 @@ export async function CategoriesForm() {
           </div>
           <ToggleAllButton title="Toggle all categories" />
         </form>
-        <p className="text-sm m-4 text-gray-200">
+        <p className="text-sm m-4 text-slate-900 dark:text-gray-200">
           Please note: some categories contain only a few websites at the
           moment. It&apos;s recommended to select more categories to start with.
         </p>
