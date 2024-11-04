38d1b17809a5670ae6fe48e8942914a86f5e5be3
kei-en <njuguna0karanja@gmail.com>
2024-11-04 14:40:59 +0300

    Fix typos and stylistic issues to handle user preference

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -8,7 +8,7 @@ import { Textarea } from "./ui/textarea";
 
 export function ContactForm() {
   return (
-    <div className="w-[95%] lg:w-5/6 mx-auto border border-slate-800 mb-2 md:mb-4 rounded-md md:rounded-lg p-4 pt-2 px-10 md:p-8 md:pt-4 md:px-16 shadow-input bg-slate-950 opacity-95">
+    <div className="w-[95%] lg:w-5/6 mx-auto border border-slate-800 mb-2 md:mb-4 rounded-md md:rounded-lg p-4 pt-2 px-10 md:p-8 md:pt-4 md:px-16 shadow-input bg-slate-300 dark:bg-slate-950 opacity-95">
       <div className="my-8 lg:m-8 w-full">
         <h2 className="text-2xl font-semibold py-4">Contact</h2>
         <p className="">
@@ -58,9 +58,9 @@ export function ContactForm() {
           />
         </LabelInputContainer>
         <button
-          className="bg-gradient-to-br relative group/btn from-slate-950 via-slate-800 dark:from-slate-600 dark:via-slate-800 dark:to-slate-600 to-slate-800 block dark:bg-slate-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="bg-gradient-to-br relative group/btn from-slate-400 via-slate-300 dark:from-slate-600 dark:via-slate-800 dark:to-slate-600 to-slate-400 block dark:bg-slate-800 w-full rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit">
-          Submit site &rarr;
+          Submit &rarr;
           <BottomGradient />
         </button>
       </form>
