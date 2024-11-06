7e8e071979508582e7bdf3da86d6ad25c0c9556e
kei-en <njuguna0karanja@gmail.com>
2024-11-06 17:46:56 +0300

    Add id's to site data, uuid package

--- data.js
+++ data.js
@@ -1,5 +1,3 @@
-import { title } from "process";
-
 export const categories = [
   {
     id: "art",
@@ -8,6 +6,7 @@ export const categories = [
     color: "purple",
     sites: [
       {
+        id: "0f601b34",
         title: "A Rare Look at a Korean Portraiture Tradition",
         description:
           "An exhibition at the Asian Art Museum is only the latest step in a long journey to chart the development of Korean identity through art.",
@@ -15,6 +14,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "0efe005e",
         title: "The Shape of AI",
         description:
           "Exploring how User Experience will evolve with the growth of Artificial Intelligence.",
@@ -22,12 +22,14 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "f8db72a3",
         title: "Totally rocking websites | httpster",
         description: "Web design inspiration from all over the world.",
         slug: "https://httpster.net/",
         submitter: "JonCarey",
       },
       {
+        id: "da9ba37f",
         title: "Aesthetics Wiki | Fandom",
         description:
           "Our wiki is a comprehensive encyclopedia of online and offline aesthetics!",
@@ -35,6 +37,7 @@ export const categories = [
         submitter: "Anonymous User",
       },
       {
+        id: "92d48542",
         title: "List of works based on dreams - Wikipedia",
         description:
           "Dreams have been credited as the inspiration for several creative works and scientific discoveries.",
@@ -42,12 +45,14 @@ export const categories = [
         submitter: "Kei eN",
       },
       {
+        id: "d58bfa8f",
         title: "Biro + Banana = Art 🍌🖊️",
         description: "A gallery of ballpoint-on-fruit masterpieces.",
         slug: "https://vole.wtf/biro-banana/",
         submitter: "Anonymous User",
       },
       {
+        id: "ad62f710",
         title:
           "Amigurumio - Online Amigurumi Pattern Generator & Crochet Patterns",
         description:
@@ -56,6 +61,7 @@ export const categories = [
         submitter: "greenmountainmagpie",
       },
       {
+        id: "64327e11",
         title: "UX/UI Product Design Roadmap",
         description:
           "UX/UI Product Design Roadmap is a diagram that outlines the main design topics and skills you need to know as a UX/UI Product designer in 2023.",
@@ -63,12 +69,14 @@ export const categories = [
         submitter: "ShashankPoojari",
       },
       {
+        id: "0d5cacf5",
         title: "The Bézier Game",
         description: "A game to help you master the pen tool.",
         slug: "https://bezier.method.ac/",
         submitter: "Goitbit",
       },
       {
+        id: "8ec1b760",
         title: "History of Icons – a visual brief on icon history by FUTURAMO",
         description:
           "History of computer icons through different graphical user interfaces in different operating systems.",
@@ -76,12 +84,14 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "a3162374",
         title: "Arthur.io • A Digital Museum",
         description: "Arthur is a place to collect and discover art.",
         slug: "https://arthur.io/",
         submitter: "greenmountainmagpie",
       },
       {
+        id: "3f886090",
         title: "Spraydaily.com - Daily news from the graffiti world!",
         description: "Daily news from the graffiti world!",
         slug: "https://www.spraydaily.com/",
@@ -96,6 +106,7 @@ export const categories = [
     color: "orange",
     sites: [
       {
+        id: "b0640936",
         title: "The Art and Science of Spending Money · Collab Fund",
         description:
           "Former General Electric CEO Jack Welch once nearly died of a heart attack.",
@@ -103,12 +114,14 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "4d460565",
         title: "Developer marketing examples",
         description: "",
         slug: "https://www.developermarkepear.com/developer-marketing-examples",
         submitter: "ShashankPoojari",
       },
       {
+        id: "7a64341f",
         title:
           "The Deep and Enduring History of Universal Basic Income | The MIT Press Reader",
         description:
@@ -117,6 +130,7 @@ export const categories = [
         submitter: "ShashankPoojari",
       },
       {
+        id: "4aa3c3c3",
         title:
           "How successful companies got their first users – Early User Growth",
         description:
@@ -125,6 +139,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "193e7563",
         title: "Boomer Apple – Stratechery by Ben Thompson",
         description:
           "Apple is well and truly a services company; hardware is necessary but insufficient for future growth.",
@@ -132,6 +147,7 @@ export const categories = [
         submitter: "Anonymous User",
       },
       {
+        id: "f160eb15",
         title: "Tom Blomfield",
         description:
           "Partner at Y Combinator. Co-founded Monzo and GoCardless. Based in San Francisco.",
@@ -139,6 +155,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "9d06bd86",
         title:
           "A Wealth of Common Sense - Personal Finance, Investments & Markets",
         description: "Personal Finance, Investments & Markets.",
@@ -146,6 +163,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "74d76eb0",
         title: "Dark Design Patterns Catalog",
         description:
           "Regulations from the Government of India prohibit 12 common dark design patterns. These sneaky practices are unethical applications of established UX knowledge to make interface designs that...",
@@ -153,12 +171,14 @@ export const categories = [
         submitter: "Anonymous User",
       },
       {
+        id: "3d033753",
         title: "PUBLIC INTERVENTIONS - the Languages of Media Course Text",
         description: "",
         slug: "https://books.lib.uoguelph.ca/languagesofmedia/chapter/chapter-7-public-interventions/",
         submitter: "Kovah",
       },
       {
+        id: "1ad81c3c",
         title: "SUCCESS | Your Trusted Guide to the Future of Work",
         description:
           "SUCCESS is your guide for personal and professional development through inspiration, motivation and training.",
@@ -174,6 +194,7 @@ export const categories = [
     color: "yellow",
     sites: [
       {
+        id: "c9d87cc2",
         title: "Life Stats",
         description:
           "Find out what has happened in your lifetime. How many times have you blinked? How much has the stock market gone up?",
@@ -181,6 +202,7 @@ export const categories = [
         submitter: "Anonymous User",
       },
       {
+        id: "5700f479",
         title: "NOIYS - Post, read, forget",
         description:
           "The anti-social network. Post a note anonymously. It's viewed by many, then deleted after 24 hours.",
@@ -188,12 +210,14 @@ export const categories = [
         submitter: "ShashankPoojari",
       },
       {
+        id: "8689115b",
         title: "mi kama sona e toki pona or something",
         description: "",
         slug: "https://cohost.org/mcc/post/59045-mi-kama-sona-e-toki",
         submitter: "Kovah",
       },
       {
+        id: "b5b9d5ac",
         title: "The Beirut Bank Job – Darknet Diaries",
         description:
           "Jayson E. Street tells us a story about the time he broke into a bank in Beirut Lebanon.",
@@ -201,6 +225,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "ab68d19a",
         title:
           "Solo Poly | Life and love from way, way off the Relationship Escalator",
         description:
@@ -209,6 +234,7 @@ export const categories = [
         submitter: "SOBQJMV",
       },
       {
+        id: "9f8e9683",
         title: "7travels.net - Organize, track & share your travels!",
         description:
           "7travels.net helps you plan, document and share your travels around the world. Create a travel log to keep track of visited countries, write reviews and show visited countries on your personal...",
@@ -216,6 +242,7 @@ export const categories = [
         submitter: "SOBQJMV",
       },
       {
+        id: "76c7197d",
         title: "Occasional Radio by Lorenzo Mason Studio",
         description:
           "Lorenzo Mason Studio is a multidisciplinary creative practice engaged in design, research, production, and education.",
@@ -223,12 +250,14 @@ export const categories = [
         submitter: "ShashankPoojari",
       },
       {
+        id: "229c0d30",
         title: "If It Were My Home",
         description: "",
         slug: "https://www.ifitweremyhome.com/",
         submitter: "Kovah",
       },
       {
+        id: "f2260b42",
         title: "The pitfalls of purpose — Alex Murrell",
         description:
           "Purpose is polarising, with those who promote it and those who refute it. Successful purposes avoid these pitfalls by building a purpose on a role their product actually performs...",
@@ -236,6 +265,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "57d90b1f",
         title:
           "Your Mystery: Have Attention Spans Been Declining? – SLIME MOLD TIME MOLD",
         description:
@@ -252,6 +282,7 @@ export const categories = [
     color: "teal",
     sites: [
       {
+        id: "fc0e8ed4",
         title: "OpenStax",
         description:
           "OpenStax offers free college textbooks for all types of students, making education accessible affordable for everyone. Browse our list of available subjects!",
@@ -259,6 +290,7 @@ export const categories = [
         submitter: "Anonymous User",
       },
       {
+        id: "dab95d69",
         title: "Localingual: listen to the voices of the world",
         description:
           "Localingual is a site for listening to and recording dialects/languages around the world",
@@ -266,6 +298,7 @@ export const categories = [
         submitter: "Kovah",
       },
       {
+        id: "d1f5a89f",
         title: "Thou shalt not commit logical fallacies",
         description:
           "A logical fallacy is a flaw in reasoning. Logical fallacies are like tricks or illusions of thought, and they're often very sneakily used by politicians and the media to fool people. Don't be fooled! This website has been designed to help you identify and call out dodgy logic wherever it may raise its ugly, incoherent head.",
@@ -273,6 +306,7 @@ export const categories = [
         submitter: "Anonymous User",
       },
       {
+        id: "8eac4c75",
         title: "The Priming Effect: How Expectations Become Reality",
         description:
           "Can higher expectations actually create better outcomes? Here's what the priming effect is and how to use it to your benefit in business:",
@@ -280,12 +314,14 @@ export const categories = [
         submitter: "ShashankPoojari",
       },
       {
+        id: "b9c2a694",
         title: "A New Way to Store Knowledge",
         description: "HTML | TXT | PDF",
         slug: "https://breckyunits.com/scrollsets.html",
         submitter: "Anonymous User",
       },
       {
+        id: "0196e040",
         title:
           "What's the Difference? - A Newsletter for the Curious and Confused",
         description: "A Newsletter for the Curious and Confused",
@@ -293,6 +329,7 @@ export const categories = [
         submitter: "peric71185",
       },
       {
+        id: "70d7aa88",
         title:
           "Why language might be the optimal self-regulating system | Aeon Essays",
         description: "",
@@ -300,6 +337,7 @@ export const categories = [
         submitter: "ShashankPoojari",
       },
       {
+        id: "0a8f634b",
         title: "Online College Courses & Classes",
         description:
           "Free online courses, videos and lectures from the world's leading universities like Yale, MIT, Stanford, and Harvard. Over 500 courses right at your fingertips.",
@@ -307,12 +345,14 @@ export const categories = [
         submitter: "greenmountainmagpie",
       },
       {
+        id: "da324a4d",
         title: "Memorable people 3501 BC - 2023",
         description: "",
         slug: "https://pantheon.world/explore/rankings",
         submitter: "navchandar",
       },
       {
+        id: "b8481900",
         title: "LanguageGuide.org",
         description:
           "Explore the world of vocabulary with 80 theme based pages.",
@@ -320,11 +360,100 @@ export const categories = [
         submitter: "Anonymous User",
       },
       // {
+      // id: uuidv4(),
       //   title: "Morse - Learn",
       //   description: "Learn Morse code with Google's Morse code learning tool",
       //   slug: "https://morse.withgoogle.com/learn",
       //   submitter: "purplerib",
       // },
+    ],
+  },
+  {
+    id: "food",
+    title: "Food, Cooking",
+    description: "Cuisines, Baking, Nutrition, Vegan,...",
+    color: "lime",
+    sites: [
+      {
+        id: "6f796a56",
+        title:
+          "Why Some of the World’s Most Famous Chefs Don’t Want a Michelin Star | Vanity Fair",
+        description:
+          "The kings of top kitchens reveal what it takes to gain (and keep) the top restaurant honor—and the trauma of losing it.",
+        slug: "https://www.vanityfair.com/culture/2015/09/top-chefs-michelin-stars",
+        submitter: "Kovah",
+      },
+      {
+        id: "91d87aaf",
+        title: "I Need Coffee - By Coffee Fans, For Coffee Fans!",
+        description:
+          "INeedCoffee has been online providing coffee education and entertainment since 1999. Check out our Coffee Brewing Guide.",
+        slug: "https://ineedcoffee.com/",
+        submitter: "Kovah",
+      },
+      {
+        id: "5317f40a",
+        title: "When did it Happen?  Restaurant Franchise Timeline",
+        description:
+          "Restaurant Franchise Timeline Compiled by Dacy Nottingham.",
+        slug: "https://whendidithappen.com/restaurants/",
+        submitter: "Kovah",
+      },
+      {
+        id: "3497e70d",
+        title: "Snack Data - The Definitive Snack Database",
+        description:
+          "The definitive snack resource for snack enthusiasts throughout the World.",
+        slug: "https://snackdata.com/",
+        submitter: "ShashankPoojari",
+      },
+      {
+        id: "19a93d47",
+        title: "The Sifter - A Food History Research Tool",
+        description: "",
+        slug: "https://thesifter.org/",
+        submitter: "netsherpa",
+      },
+      {
+        id: "37cd98bf",
+        title: "The Sifter - A Food History Research Tool",
+        description:
+          "Our kitchen is always open. Grab a snack, pick up some food tips, browse our recipes and share yours!",
+        slug: "https://www.masterstech-home.com/The_Kitchen/TheKitchen.html",
+        submitter: "netsherpa",
+      },
+      {
+        id: "a09ba0e0",
+        title: "Wine & Math: A model pairing",
+        description:
+          "Mention predictive modeling to the general public and you’re likely to conjure memes of complex mathematical equations swirling. Mention wine, and you get a much different reaction...",
+        slug: "https://pudding.cool/2021/03/wine-model/",
+        submitter: "Kei eN",
+      },
+      {
+        id: "78e7420c",
+        title: "Encyclopedia of food & nutrition focused on comparison",
+        description:
+          "Evidence-based encyclopedia of food. In-depth, full data on 1000+ common foods based on nutrition and comparison. Visual representation of food through charts.",
+        slug: "https://foodstruct.com/",
+        submitter: "Anon",
+      },
+      {
+        id: "14c40a3b",
+        title: "Whale On Toast",
+        description:
+          "For much of the time that humans hunted whales, we hunted them mostly for margarine.",
+        slug: "https://www.scopeofwork.net/whale-on-toast/",
+        submitter: "Kovah",
+      },
+      {
+        id: "6d25f634",
+        title: "Unified Sandwich Framework",
+        description:
+          "Is a Pop-tart a sandwich? A simple question, but any answer must probe the very essence of what it means to be a sandwich. The critical question is rather how sandwichy is a Pop-tart, and...",
+        slug: "https://sandwich.one/",
+        submitter: "Anon",
+      },
     ],
   },
 ];
@@ -361,12 +490,12 @@ export const rules = {
     {
       title: "Low quality content or news articles",
       description:
-        "News articles, boring blog posts and other low-quality content is nothing what we want on Websurfer. \\nExceptions may apply.\\n Examples: Buzzfeed articles",
+        "News articles, boring blog posts and other low-quality content is nothing what we want on Websurfer. Exceptions may apply. Examples: Buzzfeed articles",
     },
     {
       title: "Political or highly controversial content",
       description:
-        "If you want to discuss political topics, please go to Facebook. Also,  is not a place to publish your absurd conspiracy theories.",
+        "If you want to discuss political topics, please go to Facebook. Also, Websurfer is not a place to publish your absurd conspiracy theories.",
     },
     {
       title: "Social network posts",
@@ -418,7 +547,7 @@ export const faqs = [
       "Websurfer is all about quality content without the filler. Say goodbye to news articles, social media clutter, and clickbait nonsense, and welcome a world of creative, nostalgic, and intriguing discoveries.",
   },
   {
-    title: "How can I submit a website to Cloudhiker?",
+    title: "How can I submit a website to Websurfer?",
     description:
       "If it's not already in the database, you can submit websites from the submission page.",
   },
